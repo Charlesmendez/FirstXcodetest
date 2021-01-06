7d748d4c57a835b80f15418bd1cda5c83d98f573
Charlesmendez <charlesmendez@hotmail.com>
2021-01-06 10:59:32 -0500

    Initial Commit

---
+++ Sample/SampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleApp.swift
+//  Sample
+//
+//  Created by Carlos Fernando Mendez Solano on 1/6/21.
+//
+
+import SwiftUI
+
+@main
+struct SampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
