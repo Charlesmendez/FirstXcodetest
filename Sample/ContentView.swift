ebc7c7df6dc2b34b473a419fe8a3b0f7b6881a72
Charlesmendez <charlesmendez@hotmail.com>
2021-01-16 12:58:41 -0500

    Added text in Hello World

--- Sample/ContentView.swift
+++ Sample/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        Text("Hello, world, this is a git test!")
             .padding()
     }
 }
